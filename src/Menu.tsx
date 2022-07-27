1c9137769354268f757ab125fc74d7b78b096411
Fredricc <kfred45.fk@gmail.com>
2022-07-27 15:21:20 +0300

    Added Nav bar

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -4,13 +4,13 @@ export default function Menu(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <NavLink className= "navbar-brand" to="/">React Movies</NavLink>
+                <a className= "navbar-brand" href="/">React Movies</a>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-lg-0">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/genres">
+                            <a className="nav-link" href="/genres">
                                 Genres
-                            </NavLink>
+                            </a>
                         </li>
 
                     </ul>
--- src/Movies/MoviesList.tsx
+++ src/Movies/MoviesList.tsx
@@ -1,11 +1,19 @@
-//import GenericList from "./GenericList";
 import IndividualMovies from "./IndividualMovies";
 import { movieDTO } from "./movies.model";
 import css from './MoviesList.module.css';
+//import GenericList from "./utils/GenericList";
 import Loading from "./utils/Loading";
 
 
 export default function MoviesList(props: moviesListProps){
+
+    // return <GenericList list={props.movies}>
+    //     <div className={css.div}>
+    //     {props.movies?.map(movie => 
+    //     <IndividualMovies {...movie} key={movie.id} />)}
+    //     </div>
+    // </GenericList>
+    
     if(!props.movies){
         return<Loading />
     } else if (props.movies.length === 0){
