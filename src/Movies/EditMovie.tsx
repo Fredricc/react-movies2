54c434e6fe71dfb6c98c9a6c4f3df9ce4cc0e839
Fredricc <kfred45.fk@gmail.com>
2022-08-17 16:03:03 +0300

    Relating Movies with Genres and Movie Theaters

--- src/Movies/EditMovie.tsx
+++ src/Movies/EditMovie.tsx
@@ -1,6 +1,19 @@
+import { genreDTO } from "../Genres/genres.model";
+import { movieTheaterDTO } from "../moviesTheaters/movieTheater.model";
 import MovieForm from "./MovieForm";
 
 export default function EditMovie(){
+
+    const nonSelectedGenres: genreDTO[] = [{id: 2, name: 'Drama'}]
+    const selectedGenres: genreDTO[] = [{id: 1, name: 'Comedy'}]
+
+    const nonSelectedMovieTheaters: movieTheaterDTO[] = 
+    [{id: 2, name: 'Agora'}]
+
+    const selectedMovieTheaters: movieTheaterDTO[] = 
+    [{id: 1, name: 'Sambil'}]
+
+
     return(
         <>
         <h3>Edit Movie</h3>
@@ -9,6 +22,11 @@ export default function EditMovie(){
         releaseDate: new Date('2019-01-01T00:00:00')    
     }}
             onSubmit={values => console.log(values)}
+            nonSelectedGenres={nonSelectedGenres}
+            selectedGenres={selectedGenres}
+
+            nonSelectedMovieTheaters={nonSelectedMovieTheaters}
+            selectedMovieTheaters={nonSelectedMovieTheaters}
 
         />
         
