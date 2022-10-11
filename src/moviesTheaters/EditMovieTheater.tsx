2de329073e0500686cbe13a5f60bf9d0eb350ffd
Fredricc <kfred45.fk@gmail.com>
2022-10-11 07:54:25 +0300

    Editing and Deleting Movie Theaters

--- src/moviesTheaters/EditMovieTheater.tsx
+++ src/moviesTheaters/EditMovieTheater.tsx
@@ -1,17 +1,18 @@
-import MovieTheaterForm from "./MovieTheaterForm";
+import { urlMovieTheaters } from '../endpoints';
+import EditEntity from '../utils/EditEntity';
+import { movieTheaterCreationDTO, movieTheaterDTO } from './movieTheater.model';
+import MovieTheaterForm from './MovieTheaterForm';
 
 export default function EditMovieTheater(){
-    return(
-        <>
-        <h3>Edit Movie Theaters</h3>
-        <MovieTheaterForm
-            model={{name: 'Sambil'}}
-            onSubmit={values => console.log(values)}
-            />
-        
-        </>
+    return (
+        <EditEntity<movieTheaterCreationDTO, movieTheaterDTO>
+            url={urlMovieTheaters} indexURL="/movietheaters" entityName="Movie Theater"
+        >
+            {(entity, edit) => 
+                <MovieTheaterForm model={entity}
+                 onSubmit={async values => await edit(values)}
+                />
+            }
+        </EditEntity>
     )
 }
-
-
-
--- src/moviesTheaters/IndexMovieTheaters.tsx
+++ src/moviesTheaters/IndexMovieTheaters.tsx
@@ -1,10 +1,31 @@
-import { Link } from "react-router-dom";
+import { urlMovieTheaters } from "../endpoints";
+import IndexEntity from "../utils/IndexEntity";
+import { movieTheaterDTO } from "./movieTheater.model";
 
 export default function IndexMovieTheaters(){
-    return(
-    <>
-        <h3>Movie Theater</h3>
-        <Link className="btn btn-primary" to="/movieTheaters/create">Create Theater</Link>
-    </>
+    return (
+        <IndexEntity<movieTheaterDTO> 
+            url={urlMovieTheaters} createURL="movietheaters/create" title="Movie Theaters"
+            entityName="Movie Theater"
+        >
+            {(entities, buttons) => <>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {entities?.map(entity => <tr key={entity.id}>
+                        <td>
+                            {buttons(`movietheaters/edit/${entity.id}`, entity.id)}
+                        </td>
+                        <td>
+                            {entity.name}
+                        </td>
+                    </tr>)}
+                </tbody>
+            </>}
+        </IndexEntity>
     )
 }
