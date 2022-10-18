869a7bbfbeeb901cdcaf56d9552198d2f77dabac
Fredricc <kfred45.fk@gmail.com>
2022-10-18 06:12:54 +0300

    Using Claims in our React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,51 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Menu from './Menu';
-import routes from './Movies/RouteComponents/RouteConfig';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import configureValidations from './Validations';
+import { useEffect, useState } from 'react';
+import routes from './Movies/RouteComponents/RouteConfig';
+import { claim } from './auth/auth.models';
+import AuthenticationContext from './auth/AuthenticationContext';
+import { getClaims } from './auth/handleJWT';
+import configureInterceptor from './utils/httpInterceptors';
 
 configureValidations();
+configureInterceptor();
 
 function App() {
-  
 
-  return(
+  const [claims, setClaims] = useState<claim[]>([]);
+
+  useEffect(() => {
+    setClaims(getClaims())
+  }, [])
+
+  function isAdmin(){
+    return claims.findIndex(claim => claim.name === 'role' && claim.value === 'admin') > -1;
+  }
+
+  return (
     <BrowserRouter>
-    <Menu />
-    <div className='container'>
-      <Switch>
-        {routes.map(route =>
-          <Route key={route.path} path={route.path} exact={route.exact}>
-            <route.component />
-          </Route>
-          )}
-      </Switch>
-    </div>
-    <footer className='bd-footer py-p mt-5 bg-light'>
-      <div className='container'>
-        <div className='mb-10'>
-        React Movies {new Date().getFullYear().toString()}
-        <div className="card-group">
-  <div className="card">
-    
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div className="card-footer">
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div className="card">
-    
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div className="card-footer">
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div className="card">
-    
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div className="card-footer">
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
+      <AuthenticationContext.Provider value={{ claims, update: setClaims }}>
+        <Menu />
+        <div className="container">
+          <Switch>
+            {routes.map(route =>
+              <Route key={route.path} path={route.path} exact={route.exact}>
+                {route.isAdmin && !isAdmin() ? <>
+                  You are not allowed to see this page
+                </>:  <route.component />}
+              </Route>)}
+          </Switch>
         </div>
-      </div>
-    </footer>
+        <footer className="bd-footer py-5 mt-5 bg-light">
+          <div className="container">
+            React Movies {new Date().getFullYear().toString()}
+          </div>
+        </footer>
+      </AuthenticationContext.Provider>
     </BrowserRouter>
   )
 }
 
 export default App;
-
